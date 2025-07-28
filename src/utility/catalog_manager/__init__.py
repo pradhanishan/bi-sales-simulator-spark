ea9fb8e5d7f739be0ca7162e657620e621cf454e
Ishan Pradhan <ishanpradhan.ip@gmail.com>
2025-07-28 16:05:17 +0700

    add relative path builder

---
+++ src/utility/catalog_manager/__init__.py
@@ -0,0 +1,2 @@
+from .catalog_builder import CatalogBuilder, CatalogSection
+from .database_builder import DatabaseBuilder, DatabaseSection
--- src/utility/session_manager/spark_session.py
+++ src/utility/session_manager/spark_session.py
@@ -1,40 +1,32 @@
 from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 from typing import Optional
-from pathlib import Path
-from delta import *
+from delta import configure_spark_with_delta_pip
+
+from utility.path_manager import PathBuilder
+
 
 class SparkSessionFactory:
     """
     Factory class for creating and managing a singleton SparkSession instance.
-    
-    Loads environment variables, resolves project root paths,
-    configures logging using log4j.properties, and ensures the logs directory exists.
     """
 
     def __init__(self) -> None:
-        """
-        Initialize the SparkSessionFactory with resolved paths for the project root,
-        log4j configuration, and logs directory.
-        """
         load_dotenv()
         self._spark_session: Optional[SparkSession] = None
-        self.project_root = Path(__file__).resolve().parents[2]
+
+        self.path_builder = PathBuilder()
+        self.project_root = self.path_builder.get_root_directory()
         self.log4j_config_path = self.project_root / "log4j.properties"
-        self.logs_dir = self.project_root / "logs"
+        self.logs_dir = self.path_builder.get_log_directory()
         self.logs_dir.mkdir(parents=True, exist_ok=True)
 
     def get_or_create_spark_session(self) -> SparkSession:
-        """
-        Return an existing SparkSession or create a new one with custom logging configuration.
-        
-        Returns:
-            SparkSession: A configured Spark session instance.
-        """
         if self._spark_session is not None:
             return self._spark_session
 
-        builder = (SparkSession.builder
+        builder = (
+            SparkSession.builder
             .appName("bi_sales_simulator")
             .master("local[3]")
             .enableHiveSupport()
@@ -42,9 +34,10 @@ class SparkSessionFactory:
                 "spark.driver.extraJavaOptions",
                 f"-Dlog4j.configuration=file:{self.log4j_config_path} -Dlog.path={self.logs_dir}"
             )
-            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog"))
-        
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+        )
+
         self._spark_session = configure_spark_with_delta_pip(builder).getOrCreate()
 
         self._spark_session._jvm.org.apache.log4j.LogManager.getLogger("bi.application.bi_sales_simulator") \
